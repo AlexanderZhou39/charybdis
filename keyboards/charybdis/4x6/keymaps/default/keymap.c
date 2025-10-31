dd73999260f5cf870f1597bcd65b9a67aae28520
Alex <alexzhou3999@gmail.com>
2025-10-30 17:56:03 -0700

    remap game

--- keyboards/charybdis/4x6/keymaps/default/keymap.c
+++ keyboards/charybdis/4x6/keymaps/default/keymap.c
@@ -26,8 +26,10 @@ enum charybdis_keymap_layers {
 };
 
 #define LOWER MO(LAYER_LOWER)
+#define LOWER2 MO(LAYER_RIGHT)
 #define RAISE MO(LAYER_RAISE)
 #define P_TAP LT(LAYER_POINT, KC_A)
+#define TG_GAME TG(LAYER_GAME)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_ESC,    P_TAP,   KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_QUOT,
+        KC_ESC,    P_TAP,   KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, KC_LALT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LGUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT,
+       KC_LGUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, TG_GAME,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    KC_TAB, KC_SPC,   LOWER,        RAISE,  KC_LSFT,
                                            KC_LCTL,  KC_BSPC,      KC_ENT
@@ -93,31 +95,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_GAME] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+        KC_CAPS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
+        KC_TAB,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPLY, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
+        KC_M,      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPRV, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+        KC_H,      KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME, TG_GAME,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______, XXXXXXX,    _______, XXXXXXX,
-                                           _______, _______,    XXXXXXX
+                                   KC_TAB, KC_SPC,   LOWER2,    XXXXXXX, XXXXXXX,
+                                           KC_LCTL,  KC_BSPC,   XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
   [LAYER_RIGHT] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+        KC_CAPS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MNXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
+        KC_TAB,    KC_TAB,  KC_H,    KC_W,    KC_Y,    KC_U,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPLY, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_MUTE,
+        KC_M,      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_I,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_MPRV, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+        KC_H,      KC_LCTL, KC_Z,    KC_X,    KC_P,    KC_O,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  _______, _______, XXXXXXX,    _______, XXXXXXX,
-                                           _______, _______,    XXXXXXX
+                                   KC_TAB, KC_SPC,   XXXXXXX,   XXXXXXX, XXXXXXX,
+                                           KC_LCTL,  KC_BSPC,   XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 };
